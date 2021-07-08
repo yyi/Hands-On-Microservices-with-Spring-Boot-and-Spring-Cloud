f2dd85a83f63922ba7b297e5b56530e02fc68463
yyi <yyi.mailer@gmail.com>
2021-07-08 11:08:34 +0800

    监控指标版本新增，去除deprecated

--- Chapter07/microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
+++ Chapter07/microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -3,20 +3,22 @@ package se.magnus.microservices.composite.product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.health.*;
+import org.springframework.boot.actuate.health.CompositeReactiveHealthContributor;
+import org.springframework.boot.actuate.health.ReactiveHealthContributor;
+import org.springframework.boot.actuate.health.ReactiveHealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpMethod;
 import se.magnus.microservices.composite.product.services.ProductCompositeIntegration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spring.web.plugins.Docket;
 
-import java.util.LinkedHashMap;
+import java.util.Map;
 
 import static java.util.Collections.emptyList;
-import static org.springframework.web.bind.annotation.RequestMethod.*;
 import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
 import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
@@ -24,63 +26,69 @@ import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 @ComponentScan("se.magnus")
 public class ProductCompositeServiceApplication {
 
-    @Value("${api.common.version}")           String apiVersion;
-    @Value("${api.common.title}")             String apiTitle;
-    @Value("${api.common.description}")       String apiDescription;
-    @Value("${api.common.termsOfServiceUrl}") String apiTermsOfServiceUrl;
-    @Value("${api.common.license}")           String apiLicense;
-    @Value("${api.common.licenseUrl}")        String apiLicenseUrl;
-    @Value("${api.common.contact.name}")      String apiContactName;
-    @Value("${api.common.contact.url}")       String apiContactUrl;
-    @Value("${api.common.contact.email}")     String apiContactEmail;
+    @Value("${api.common.version}")
+    String apiVersion;
+    @Value("${api.common.title}")
+    String apiTitle;
+    @Value("${api.common.description}")
+    String apiDescription;
+    @Value("${api.common.termsOfServiceUrl}")
+    String apiTermsOfServiceUrl;
+    @Value("${api.common.license}")
+    String apiLicense;
+    @Value("${api.common.licenseUrl}")
+    String apiLicenseUrl;
+    @Value("${api.common.contact.name}")
+    String apiContactName;
+    @Value("${api.common.contact.url}")
+    String apiContactUrl;
+    @Value("${api.common.contact.email}")
+    String apiContactEmail;
 
-	/**
-	 * Will exposed on $HOST:$PORT/swagger-ui.html
-	 *
-	 * @return
-	 */
-	@Bean
-	public Docket apiDocumentation() {
+    /**
+     * Will exposed on $HOST:$PORT/swagger-ui.html
+     *
+     * @return
+     */
+    @Bean
+    public Docket apiDocumentation() {
 
-		return new Docket(SWAGGER_2)
-			.select()
-			.apis(basePackage("se.magnus.microservices.composite.product"))
-			.paths(PathSelectors.any())
-			.build()
-				.globalResponseMessage(POST, emptyList())
-				.globalResponseMessage(GET, emptyList())
-				.globalResponseMessage(DELETE, emptyList())
-				.apiInfo(new ApiInfo(
-                    apiTitle,
-                    apiDescription,
-                    apiVersion,
-                    apiTermsOfServiceUrl,
-                    new Contact(apiContactName, apiContactUrl, apiContactEmail),
-                    apiLicense,
-                    apiLicenseUrl,
-                    emptyList()
+        return new Docket(SWAGGER_2)
+                .select()
+                .apis(basePackage("se.magnus.microservices.composite.product"))
+                .paths(PathSelectors.any())
+                .build()
+                .globalResponses(HttpMethod.GET, emptyList())
+                .globalResponses(HttpMethod.POST, emptyList())
+                .globalResponses(HttpMethod.DELETE, emptyList())
+                .apiInfo(new ApiInfo(
+                        apiTitle,
+                        apiDescription,
+                        apiVersion,
+                        apiTermsOfServiceUrl,
+                        new Contact(apiContactName, apiContactUrl, apiContactEmail),
+                        apiLicense,
+                        apiLicenseUrl,
+                        emptyList()
                 ));
     }
 
-	@Autowired
-	HealthAggregator healthAggregator;
+    @Autowired
+    ProductCompositeIntegration integration;
 
-	@Autowired
-	ProductCompositeIntegration integration;
+    @Bean
+    ReactiveHealthContributor coreServices() {
 
-	@Bean
-	ReactiveHealthIndicator coreServices() {
 
-		ReactiveHealthIndicatorRegistry registry = new DefaultReactiveHealthIndicatorRegistry(new LinkedHashMap<>());
+        return CompositeReactiveHealthContributor.fromMap(Map.of(
+                "product", (ReactiveHealthIndicator) () -> integration.getProductHealth(),
+                "recommendation", () -> integration.getRecommendationHealth(),
+                "review", () -> integration.getReviewHealth()));
 
-		registry.register("product", () -> integration.getProductHealth());
-		registry.register("recommendation", () -> integration.getRecommendationHealth());
-		registry.register("review", () -> integration.getReviewHealth());
 
-		return new CompositeReactiveHealthIndicator(healthAggregator, registry);
-	}
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductCompositeServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCompositeServiceApplication.class, args);
+    }
 }
